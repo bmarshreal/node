46962e9ceb84cbac85717caaf47d0ff06690e4bb
bmarshreal <bmarshreal@yahoo.com>
2020-10-27 21:00:54 -0500

    Figuring out async (non-blocking) node code

--- app.js
+++ app.js
@@ -1,44 +1,43 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http')
+const fs = require('fs')
 
+function rqListener(req, res) {}
 
-function rqListener(req,res) {
+const server = http.createServer((req, res) => {
+  const url = req.url
+  const method = req.method
+  if (url === '/') {
+    res.write('<html>')
+    res.write('<head><title>Enter Your Name please.</title></head>')
+    res.write(
+      '<body><form action = "/message" method = "POST"><input name ="message" type="text"><button type = "submit">Send</button></form></body>',
+    )
+    res.write('</html>')
+    return res.end()
+    // res.write()
+    // res.write()
+    // res.write()
+    // res.write()
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = []
+    req.on('data', (dataChunk) => {
+      body.push(dataChunk)
+      console.log(body)
+    })
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString()
+      console.log(parsedBody)
+      const message = parsedBody.split('=')[1]
+      fs.writeFile('message.txt', message, (err) => {
+        res.writeHead(302, { Location: '/' })
+        return res.end()
+      })
+    })
+  }
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<h1>New Data! aha! Welcome to my Node Server!</h1>')
+  res.end()
+})
 
-}
-
-const server = http.createServer((req,res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Your Name please.</title></head>');
-        res.write('<body><form action = "/message" method = "POST"><input name ="message" type="text"><button type = "submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-        // res.write()
-        // res.write()
-        // res.write()
-        // res.write()
-    }
-    if(url === '/message' && method ==='POST'){
-        const body = [];
-        req.on('data', (dataChunk) => {
-            body.push(dataChunk);
-            console.log(body);
-        });
-        req.on('end', ()=> {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=') [1];
-            fs.writeFileSync('message.txt', message);
-        })
-        
-        res.writeHead(302, {"Location":  "/"});
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>New Data! aha!</h1>")
-    res.end();
-});
-
-server.listen(3000);
+server.listen(3000)
